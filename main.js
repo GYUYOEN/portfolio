ac7e8f62832636096b049941cc90deeff0c8f938
김규연 <rbdus9696@gmail.com>
2023-01-06 17:00:58 +0900

    feature : 스크롤 이동 js 추가, 전체적인 디자인 조정

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.nav-link');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+const scrollMove = document.getElementById("sroll-move");
+
+scrollMove.click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+})
